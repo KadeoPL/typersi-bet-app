a579e14adb537746db2c065af45730c5acb46292
KadeoPL <stopkakrzysztof1@gmail.com>
2026-04-21 22:00:53 +0200

    add baseurl

--- utils/auth.ts
+++ utils/auth.ts
@@ -3,6 +3,7 @@ import { Pool } from "pg";
 import { username } from "better-auth/plugins";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: new Pool({
     connectionString: process.env.DATABASE_URL,
   }),
